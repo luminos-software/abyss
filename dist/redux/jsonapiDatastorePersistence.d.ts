5727a361c6423c699569b88894ac3f720b7cc2bc
Radu Batori <radubatori@luminos.software>
2018-10-18 13:45:44 +0300

    Upgrades

--- dist/redux/jsonapiDatastorePersistence.d.ts
+++ dist/redux/jsonapiDatastorePersistence.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="../../node_modules/redux-persist/lib/index.d.ts" />
 export declare const jsonapiDatastorePersistence: import("redux-persist").Transform<any, any>;
--- dist/redux/offlineReducer.d.ts
+++ dist/redux/offlineReducer.d.ts
@@ -1,6 +1,6 @@
 export declare const OfflineActions: {
-    rollback: import("typescript-fsa").EmptyActionCreator;
-    commit: import("typescript-fsa").EmptyActionCreator;
+    rollback: import("typescript-fsa").ActionCreator<void>;
+    commit: import("typescript-fsa").ActionCreator<void>;
 };
 export declare type State = Readonly<{}>;
 export declare const reducer: (state: Readonly<{}> | undefined, action: {
